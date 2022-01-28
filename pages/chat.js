ae4001cde9f726c1c58871c5f31c661fe617a026
paola-yumi <paola.yumi.m@gmail.com>
2022-01-28 00:56:28 -0300

    fifth commit

--- pages/chat.js
+++ pages/chat.js
@@ -2,6 +2,9 @@ import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import CircularProgress from '@mui/material/CircularProgress';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMwNDI4NywiZXhwIjoxOTU4ODgwMjg3fQ.Z4aRGlTSkWUSFQNGBPg-ycshMJcgFPWvrwICVpRpNIw';
 const SUPABASE_URL = 'https://nrosyolymrdzsvxkvrub.supabase.co';
@@ -123,7 +126,7 @@ export default function ChatPage() {
                         />
 
                         <Button
-                            variant='secondary'
+                            variant='tertiary'
                             colorVariant='neutral'
                             label='Send'
                             size='sm'
@@ -152,8 +155,9 @@ function Header() {
                     Chat
                 </Text>
                 <Button
-                    styleSheet={{fontFamily: 'Century Gothic'}}
-                    variant='tertiary'
+                    styleSheet={{
+                        fontFamily: 'Century Gothic'}}
+                    variant='secondary'
                     colorVariant='neutral'
                     label='Logout'
                     href="/"
@@ -171,92 +175,129 @@ function MessageList(props) {
         props.setMessage(filtered);
     }
 
-    return (
-        <Box
-            tag="ul"
+    if (props.messages.length === 0) {
+        return (
+            <Box
+            tag="main"
             styleSheet={{
-                overflow: 'scroll',
                 display: 'flex',
                 flexDirection: 'column-reverse',
                 flex: 1,
                 color: appConfig.theme.colors.personalized['blue-dark'],
-                marginBottom: '16px',
-                paddingRight: '5px'
+                margin: 'auto auto',
+                padding: '100px 100px 150px 100px'
             }}
-        >
-            {props.messages.map((message) => {
-                return(
-                    <Text
-                    key={message.id}
-                    tag="li"
-                    styleSheet={{
-                        borderRadius: '5px',
-                        padding: '6px',
-                        marginBottom: '12px',
-                        hover: {
-                            backgroundColor: appConfig.theme.colors.personalized['beige'],
-                        },
-                        fontFamily: 'Century Gothic'
-                    }}
-                    >
-                    <Box
-                        styleSheet={{
-                            marginBottom: '8px',
-                        }}
-                    >
-                        <Image
-                            styleSheet={{
-                                width: '20px',
-                                height: '20px',
-                                borderRadius: '50%',
-                                display: 'inline-block',
-                                marginRight: '8px'
-                            }}
-                            src={`https://github.com/${message.from}.png`}
-                        />
-                        <Text 
-                            tag="strong"
-                            styleSheet={{
-                                fontFamily: 'Century Gothic',
-                                fontWeight: '600',
-                                fontSize: '14px'
-                            }}
-                        >
-                            {message.from}
-                        </Text>
+            >       
+                <CircularProgress color='inherit'/>
+            </Box>
+        
+        )
+    } else {
+        return (
+            <Box
+                tag="ul"
+                styleSheet={{
+                    overflow: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column-reverse',
+                    flex: 1,
+                    color: appConfig.theme.colors.personalized['blue-dark'],
+                    marginBottom: '16px',
+                    paddingRight: '5px'
+                }}
+            >
+                {props.messages.map((message) => {
+                    return(
                         <Text
-                            styleSheet={{
-                                fontSize: '10px',
-                                marginLeft: '8px',
-                                color: appConfig.theme.colors.neutrals[300],
-                            }}
-                            tag="span"
+                        key={message.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.personalized['beige'],
+                            },
+                            fontFamily: 'Century Gothic'
+                        }}
                         >
-                            {(new Date().toLocaleDateString())}
-                        </Text>
-                        <Button
-                            iconName="times"
-                            variant='tertiary'
-                            colorVariant='neutral'
-                            size='xs'
+                        <Box 
                             styleSheet={{
-                                alignItems: 'flex-end',
-                                hover: {
-                                    backgroundColor: appConfig.theme.colors.personalized['beige'],
-                                    color: appConfig.theme.colors.personalized['blue-dark']
-                                }
+                                width: '758px',
+                                height: '20px',
+                                overflow: 'none',
+                                display: 'flex',
+                                flexDirection: 'row-reverse'
                             }}
-                            onClick={() => {
-                                message.isDeleted = true;
-                                deleteMessage(props.messages);
+                        >
+                            <Button
+                                    iconName='times'
+                                    variant='tertiary'
+                                    colorVariant='neutral'
+                                    styleSheet={{
+                                        hover: {
+                                            backgroundColor: 'transparent',
+                                            color: appConfig.theme.colors.personalized['blue-dark'],
+                                        },
+                                        focus: {
+                                            overflow: 'none',
+                                            backgroundColor: 'transparent',
+                                            color: appConfig.theme.colors.personalized['blue-dark']
+                                        },
+                                    }}
+                                    onClick={() => {
+                                        message.isDeleted = true;
+                                        deleteMessage(props.messages);
+                                    }}
+                                />
+                        </Box>
+                        <Box
+                            styleSheet={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: '10px'
                             }}
-                        />
-                    </Box>
-                    {message.text}
-                    </Text>     
-                );
-                
-            })}
-        </Box>
-    )
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '30px',
+                                    height: '30px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/${message.from}.png`}
+                            />
+                            <Text 
+                                tag="strong"
+                                styleSheet={{
+                                    fontFamily: 'Century Gothic',
+                                    fontWeight: '600',
+                                    fontSize: '14px'
+                                }}
+                            >
+                                {message.from}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                    marginBottom: '10px'
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {message.text}
+                        </Text>     
+                    );
+                    
+                })}
+            </Box>
+        )    
+    }
+    
 }
